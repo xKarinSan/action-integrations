76779d9d647f7c38192003e1a1689af783c9f634
Setsuna Yukiii <siang-meng@live.com>
2023-11-27 22:29:10 +0800

    ..

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,10 +4,10 @@ from backend.app.mongoclient import database_client
 
 import os
 
-def clean_up():
+async def clean_up():
     database_name = os.getenv("DATABASE_NAME")
     print("database_name: ",database_name)
-    database_client.drop_database(database_name)
+    await database_client.drop_database(database_name)
     
     # for entity in range(len(entities)-1, -1, -1):
     #     client.delete("/"+entities[entity]+"/deleteall")
@@ -16,9 +16,11 @@ def clean_up():
 # ========================================FastAPI test========================================
 
 
-@pytest.fixture(scope="module")
-def test_fixture():
-    clean_up()
+@pytest.fixture(scope="session")
+async def test_fixture():
+    print("Setuppp")
+    await clean_up()
     # yield will be ignored in pytest 4.0
     yield "resource"
-    clean_up()
+    await clean_up()
+    
--- backend/tests/test_create_event.py
+++ backend/tests/test_create_event.py
@@ -1,23 +1,24 @@
 from backend.tests.testconfig import api_client
-from backend.tests.test_main import test_fixture
 from datetime import datetime
 
-test_fixture
-def test_create_event_success():
+# test_fixture
+
+def test_create_event_success(test_fixture):
+    print("test_fixture",test_fixture)
     event_date = datetime.timestamp(datetime(2023, 12, 1))
     response = api_client.post("/api/event", json={"name": "Create new event test","event_date":event_date})
     assert response.status_code == 200
     assert response.json() == {"message": "Event created"}
 
 
-def test_create_event_unsuccessful_empty_name():
+def test_create_event_unsuccessful_empty_name(test_fixture):
     event_date = int(datetime.timestamp(datetime(2023, 12, 1)))
     response = api_client.post("/api/event", json={"name":"","event_date":event_date})
     assert response.status_code == 400
     assert response.json() == {"detail": "Event name is required"}
 
 
-def test_create_event_unsuccessful_empty_date():
+def test_create_event_unsuccessful_empty_date(test_fixture):
     # event_date = datetime.datetime(2023, 12, 1)
     response = api_client.post("/api/event", json={"name":"test event 1","event_date":-1})
     assert response.status_code == 400
@@ -25,7 +26,7 @@ def test_create_event_unsuccessful_empty_date():
 
 
 
-def test_create_event_unsuccessful_past_date():
+def test_create_event_unsuccessful_past_date(test_fixture):
     event_date = datetime.timestamp(datetime(2022, 12, 1))
     response = api_client.post("/api/event", json={"name": "test event","event_date":event_date})
     assert response.status_code == 400
