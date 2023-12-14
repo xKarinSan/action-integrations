efc6e28b87909eab43636e48bd22d76925b0eb1c
Setsuna Yukiii <siang-meng@live.com>
2023-12-14 17:23:02 +0800

    feat: empty placeholder

--- frontend/src/tests/components/event_container.test.tsx
+++ frontend/src/tests/components/event_container.test.tsx
@@ -3,34 +3,33 @@
 import { render,screen } from "@testing-library/react";
 import { describe, it, expect, test } from "vitest";
 import EventList from "../../components/EventList";
+
 // ======== component imports ========
 // import {EventList}
 // ======================== test cases ========================
 // case: No events
 describe("No events", () => {
-    it("Empty placeholder shows", () => {
-        const defaultProps = {
-            events :[]
-        }
+    it("Empty placeholder shows when empty", () => {
         render(<EventList events={[]} />);
-        expect(screen.getAllByTestId("noevents-placeholder").length).toBe(1);
+        expect(screen.queryAllByTestId("noevents-placeholder").length).toBe(1);
+        expect(screen.queryAllByTestId("eventlist-container").length).toBe(0);
     });
 });
 
 // === example test case ===
-export function multiply(a:Number, b:Number) {
-    return a * b;
-}
+// export function multiply(a:Number, b:Number) {
+//     return a * b;
+// }
 
-test("multiplys 1 * 2 to equal 3", () => {
-    expect(multiply(1, 2)).toBe(2);
-});
+// test("multiplys 1 * 2 to equal 3", () => {
+//     expect(multiply(1, 2)).toBe(2);
+// });
 
-describe("multiply function test", () => {
-    it("correct multiplication", () => {
-        expect(multiply(1, 2)).toBe(2);
-    });
-    it("in correct multiplication", () => {
-        expect(multiply(1, 2)).not.toBe(3);
-    });
-});
+// describe("multiply function test", () => {
+//     it("correct multiplication", () => {
+//         expect(multiply(1, 2)).toBe(2);
+//     });
+//     it("in correct multiplication", () => {
+//         expect(multiply(1, 2)).not.toBe(3);
+//     });
+// });
